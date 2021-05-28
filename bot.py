0e4372e0be3980194254d7b70631da8d4ef47c7a
DevangJain <70439930+DevangJain@users.noreply.github.com>
2021-05-28 14:15:27 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,8 +3,6 @@ from pyrogram import Client
 import os
 
 if bool(os.environ.get("WEBHOOK", False)):
-    from sample_config import Config
-else:
     from config import Config  # pylint:disable=import-error
 
 
