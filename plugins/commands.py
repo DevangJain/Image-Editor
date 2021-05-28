33b2ee8be35ebf27073eca334abe43437116aa79
DevangJain <70439930+DevangJain@users.noreply.github.com>
2021-05-28 11:39:17 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -1,12 +1,25 @@
 # By @TroJanzHEX
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
+from pyrogram.errors import UserNotParticipant, UserBannedInChannel
 from pyrogram import Client, filters
+from config import Config
 from script import script  # pylint:disable=import-error
 
 
 @Client.on_message(filters.command(["start"]) & filters.private)
 async def start(client: Client, message: Message):
-    try:
+    if Config.UPDATE_CHANNEL:
+        try:
+            user = await bot.get_chat_member(Config.UPDATE_CHANNEL, update.chat.id)
+            if user.status == "kicked":
+              await bot.edit_message_text(text=script.BANNED_USER_TEXT, message_id=fmsg.message_id)
+              return
+        except UserNotParticipant:
+            await bot.edit_message_text(chat_id=update.chat.id, text=script.FORCE_SUBSCRIBE_TEXT, message_id=fmsg.message_id, reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text="Join Channel 💻", url=f"https://telegram.me/{Config.UPDATE_CHANNEL}")]]))
+            return
+        except Exception:
+            await bot.edit_message_text(chat_id=update.chat.id, text=script.SOMETHING_WRONG, message_id=fmsg.message_id)
+            return
         await message.reply_text(
             text=script.START_MSG.format(message.from_user.mention),
             disable_web_page_preview=True,
